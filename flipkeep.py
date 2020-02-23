b7e43d8155baf1d254641cb411e2b2ceeb67b715
Your Name <you@example.com>
2020-02-24 01:20:30 +0530

    added comments

--- flipkeep.py
+++ flipkeep.py
@@ -1,18 +1,18 @@
 import random
 coin=['H','T']
-x=random.choice(coin)
+x=random.choice(coin) #tossing a coin
 
- 
+#player 1 and player to choices
 p1=input("choose to flip or keep:")
 p2=input("choose to flip or keep:")
 print("random no.",x)
 if p1=='flip':
-   if x=='H':
+   if x=='H': #swapping
      x = 'T'
    else:
      x = 'H'
 if p2=='flip':
-   if x=='H':
+   if x=='H': #swapping
      x = 'T'
    else:
      x = 'H'
